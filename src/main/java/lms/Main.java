7e5065124c3edd346fa381261f687650721f7f92
hieuminhtranwork <hieuminhtran071102@gmail.com>
2025-08-11 11:19:01 +0700

    initialize project structure with configuration files and dependencies

---
+++ src/main/java/lms/Main.java
@@ -0,0 +1,13 @@
+package lms;
+
+import org.hibernate.SessionFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        SessionFactory sessionFactory = (SessionFactory) context.getBean("sessionFactory");
+
+    }
+}
